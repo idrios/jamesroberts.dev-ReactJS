d54f664e5cb008d3ae232eb32b8957b3d214a177
Idrios <jimroberts10@gmail.com>
2020-08-31 00:54:16 -0400

    added boilerplate for unit testing with jest

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer'; 
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('page can be rendered and also 1 equals 1', () => {
+  const component = renderer.create(<App />); 
+  let tree = component.toJSON(); 
+  expect(1).toBe(1); 
 });
