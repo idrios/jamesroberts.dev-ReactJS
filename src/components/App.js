7bee5f792ee4781a105720745d5b28b921be9f69
Idrios <idrios@naiatutorials.com>
2021-04-11 01:20:52 -0400

    add bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'; 
+import { Button } from 'react-bootstrap'; 
 
 class App extends Component {
     render() {
         return (
             <div>
                 <h1>JamesRoberts.dev</h1>
+                <Button>Bootstrap button</Button>
             </div>
         ); 
     }
