136a8df8da87d5263d83f0569805b0d1ffdd7f4d
Idrios <idrios@naiatutorials.com>
2021-05-02 13:11:04 -0400

    add ability to tab through elements in header using arrow keys

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -9,12 +9,14 @@ class Header extends Component {
     constructor(props) {
         super(props);
         this.visible = this.props.visible; 
-        this.pageSrc = this.props.pageSrc;  
-        console.log(props); 
+        this.pageSrc = this.props.pageSrc;
+    }
+
+    componentDidMount(){
+        document.getElementById(this.pageSrc).focus(); 
     }
 
     render(){
-        console.log(this.pageSrc); 
         return !this.visible 
             ? <></>
             : (
@@ -25,32 +27,57 @@ class Header extends Component {
                 <div className="header-content-bottom">
                     <div className="header-content-left">
                         <Button component={ Link } to="/home" 
+                            id={PAGE_NAMES.HOME}
                             color={this.pageSrc===PAGE_NAMES.HOME ? "primary" : "default"}
                             variant={this.pageSrc===PAGE_NAMES.HOME ? "contained" : "text"}
+                            onKeyDown={ev => {
+                                if(ev.key === 'ArrowRight') document.getElementById(PAGE_NAMES.POSTS).focus()}
+                            }
                             >Home</Button>
                         <Button component={ Link } to="/posts"
+                            id={PAGE_NAMES.POSTS}
                             color={this.pageSrc===PAGE_NAMES.POSTS ? "primary" : "default"}
                             variant={this.pageSrc===PAGE_NAMES.POSTS ? "contained" : "text"}
+                            onKeyDown={ev => {
+                                if(ev.key === 'ArrowLeft') document.getElementById(PAGE_NAMES.HOME).focus(); 
+                                else if(ev.key === 'ArrowRight') document.getElementById(PAGE_NAMES.DEMOS).focus()}
+                            }
                             >Posts</Button>
                         <Button component={ Link } to="/demos"
-                            color={this.pageSrc===PAGE_NAMES.DEMOS ? "primary" : "default"}
-                            variant={this.pageSrc===PAGE_NAMES.DEMOS ? "contained" : "text"}
+                            id={PAGE_NAMES.DEMOS}
+                            color={this.pageSrc==="demos" ? "primary" : "default"}
+                            variant={this.pageSrc==="demos" ? "contained" : "text"}
+                            onKeyDown={ev => {
+                                if(ev.key === 'ArrowLeft') document.getElementById(PAGE_NAMES.POSTS).focus(); 
+                                else if(ev.key === 'ArrowRight') document.getElementById(PAGE_NAMES.ABOUT).focus()}
+                            }
                             >Demos</Button>
                         <Button component={ Link } to="/about"
-                            color={this.pageSrc===PAGE_NAMES.ABOUT ? "primary" : "default"}
-                            variant={this.pageSrc===PAGE_NAMES.ABOUT ? "contained" : "text"}
+                            id={PAGE_NAMES.ABOUT}
+                            color={this.pageSrc==="about" ? "primary" : "default"}
+                            variant={this.pageSrc==="about" ? "contained" : "text"}
+                            onKeyDown={ev => {
+                                if(ev.key === 'ArrowLeft') document.getElementById(PAGE_NAMES.DEMOS).focus(); 
+                                else if(ev.key === 'ArrowRight') document.getElementById("themes").focus()}
+                            }
                             >About</Button>
                     </div>
                     <div className="header-content-center small-screen-hidden">
                         <h1>JamesRoberts.dev</h1>
                     </div>
                     <div className="header-content-right">
-                        <Button>Themes</Button>
+                        <Button
+                            id={"themes"}
+                            onKeyDown={ev => {
+                                if(ev.key === 'ArrowLeft') document.getElementById(PAGE_NAMES.ABOUT).focus()}
+                            }
+                        >Themes</Button>
                     </div>        
                 </div>                    
             </div>
         )
     }
 }
+  
 
 export default Header; 
