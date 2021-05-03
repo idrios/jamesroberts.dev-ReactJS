412936812e6e40f177ed2f9cd7556a1d2e395514
Idrios <idrios@naiatutorials.com>
2021-05-03 00:09:10 -0400

    switch 404dogpng to 404dogjpg to make project smaller

--- src/components/Pages/404Page/404Page.js
+++ src/components/Pages/404Page/404Page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Page from '../Page/Page';
-import DogPNG from '../../../res/images/404-dog.png'; 
+import DogJPG from '../../../res/images/404-dog.jpg'; 
 import './404Page.css'; 
 
 class FourZeroFourPage extends Page {
@@ -10,7 +10,7 @@ class FourZeroFourPage extends Page {
             <>
                 <h1>404</h1>
                 <p>You are lost.</p>
-                <img src={DogPNG}></img>
+                <img src={DogJPG}></img>
             </>
         )
     }
