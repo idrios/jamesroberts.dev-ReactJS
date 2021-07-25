824ad92dc00805621379686726a47ac5c5014c65
Idrios <idrios@naiatutorials.com>
2021-07-25 13:33:07 -0400

    update the circle to be much more responsive

--- src/components/Posts/P1_11-jul-2021/Circle.js
+++ src/components/Posts/P1_11-jul-2021/Circle.js
@@ -5,103 +5,174 @@ export default class Circle extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      r: '',
-      circle: '',
-      x_scale: '',
-      y_scale: '',
+      data: {
+        r: Math.abs(this.props.r) || 0.0, 
+        x_range: this.props.x_range || [-10.0, 10.0], 
+        y_range: this.props.y_range || [-10.0, 10.0]
+      },
+      layout_params: {
+        w_px: Math.abs(this.props.w_px) || 400,
+        h_px: Math.abs(this.props.h_px) || 400,
+      },
+      d3: {
+        svg: '',
+        circle: '',
+        x_axis: '',
+        y_axis: ''
+      }
     }
   }
 
   componentDidMount() {
-    this.draw(); 
+    this.initialize(); 
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log(this.state.r); 
+  componentDidUpdate(prevProps, prevState) { 
+    if (prevProps.w_px != this.props.w_px || prevProps.h_px != this.props.h_px){
+      console.log("updating w_px to " + this.props.w_px); 
+      this.updateSvg(); 
+      this.updateAxes(); 
+      this.updateCircle(); 
+    }
+    if (prevProps.x_range != this.props.x_range || prevProps.y_range != this.props.y_range){
+      this.updateAxes(); 
+    }
     if (prevProps.r !== this.props.r) {
-       this.update();
+      this.updateCircle();
     }
  }
 
-  draw() {
-    const r = Math.abs(this.props.r) || 0.0; 
-    const xrange = this.props.xrange || [-10.0, 10.0];  
-    const yrange = this.props.yrange || [-10.0, 10.0]; 
-    const wpx = Math.abs(this.props.wpx) || 400; 
-    const wpadpx = wpx/30; 
-    const hpx = Math.abs(this.props.hpx) || 400; 
-    const hpadpx = hpx/30;
-    const rpx = (r*(wpx-(2*wpadpx)))/(xrange[1]-xrange[0]);
-
-    // Append SVG
-    const svg = d3.select(".d3-circle")
-        .append("svg")
-        .attr("width", wpx)
-        .attr("height", hpx);
-
-    // Create scale
-    const xscale = d3.scaleLinear()
-        .domain([d3.min([xrange[0], -r]), d3.max([xrange[1], r])])
-        .range([wpadpx, wpx - wpadpx]);
-
-    const yscale = d3.scaleLinear()
-        .domain(yrange)
-        .range([hpx - hpadpx, hpadpx]);
-
-    // Add scale to axes
-    const x_axis = d3.axisBottom()
-        .scale(xscale) 
-        
-    const y_axis = d3.axisLeft()
-        .scale(yscale);
-
-    const x_axis_g = svg.append("g")
-        .attr("transform", `translate(${wpx/2},0)`)
-        .call(y_axis)
-        .selectAll(".tick text")
-        .filter((elem) => elem=="0")
-        .text(""); ;
-
-    const y_axis_g = svg.append("g")
-        .attr("transform", `translate(0,${hpx/2})`)
-        .call(x_axis)
-        .selectAll(".tick text")
-        .filter((elem) => elem=="0")
-        .attr("transform", `translate(-10,0)`); 
+  initialize() {
+    const w_px = this.state.layout_params.w_px; 
+    const h_px = this.state.layout_params.h_px; 
+    const r = this.state.data.r; 
+    const x_range = this.state.data.x_range;   
+    const y_range = this.state.data.y_range; 
+    const w_pad_px = w_px/30; 
+    const h_pad_px = h_px/30;
+    const r_px = (r*(w_px-(2*w_pad_px)))/(x_range[1]-x_range[0]);
     
+    const x_scale = d3.scaleLinear()
+      .domain([d3.min([x_range[0], -r]), d3.max([x_range[1], r])])
+      .range([w_pad_px, w_px - w_pad_px]);
+    const y_scale = d3.scaleLinear()
+      .domain(y_range)
+      .range([h_px - h_pad_px, h_pad_px]);
+    const x_axis_component = d3.axisBottom()
+      .scale(x_scale) 
+    const y_axis_component = d3.axisLeft()
+      .scale(y_scale) 
+
+    const svg = d3.select(".d3-circle").append("svg")
+      .attr("width", w_px)
+      .attr("height", h_px);
+    const x_axis = svg.append("g")
+      .attr("class", "x_axis")
+      .attr("transform", `translate(0,${h_px/2})`)
+      .call(x_axis_component)
+      .selectAll(".tick text")
+      .filter((elem) => elem=="0")
+      .attr("transform", `translate(-10,0)`);
+    const y_axis = svg.append("g")
+      .attr("class", "y_axis")
+      .attr("transform", `translate(${w_px/2},0)`)
+      .call(y_axis_component)
+      .selectAll(".tick text")
+      .filter((elem) => elem=="0")
+      .text("");
     const circle = svg.append("circle")
-      .attr("cx", wpx/2)
-      .attr("cy", hpx/2)
-      .attr("r", rpx)
+      .attr("cx", w_px/2)
+      .attr("cy", h_px/2)
+      .attr("r", r_px)
       .attr("fill-opacity", "0.0")
       .attr("stroke", "red");
 
-    const text = svg.append("text")
-      .attr("transform", `translate(${wpx*0.15},${hpx*0.93})`)
-      .attr("fill", "black")
-      .attr("stroke", "black")
-      .attr("visibility", "hidden")
-      .text("You changed the size of the circle!")
-
     this.setState({
-      r,
-      circle,
-      text
-    })
+      data_params: {
+        r, 
+        x_range, 
+        y_range
+      },
+      layout_params: {
+        w_px, 
+        h_px,
+        w_pad_px,
+        h_pad_px
+      },
+      d3: {
+        svg,
+        circle,
+        x_axis,
+        y_axis
+      }
+    });
   }
 
-  update(){
-    const r = Math.abs(this.props.r) || 0.0; 
-    const xrange = this.props.xrange || [-10.0, 10.0]; 
-    const wpx = Math.abs(this.props.wpx) || 400; 
-    const wpadpx = wpx/30; 
-    const rpx = (r*(wpx-(2*wpadpx)))/(xrange[1]-xrange[0]);
-    
-    this.state.circle
-      .attr("r", rpx);
-    
-    this.state.text
-      .attr("visibility", "visible");
+  updateSvg(){
+    if(!this.state.d3.svg){
+      return; 
+    }
+    const w_px = Math.abs(this.props.w_px) || 400; 
+    const h_px = Math.abs(this.props.h_px) || 400; 
+    this.state.d3.svg
+      .attr("width", w_px)
+      .attr("height", h_px);
+  }
+
+  updateAxes(){
+    if(!this.state.d3.x_axis || !this.state.d3.y_axis){
+      return;
+    }
+    const w_px = Math.abs(this.props.w_px) || 400; 
+    const h_px = Math.abs(this.props.h_px) || 400; 
+    const w_pad_px = w_px/30; 
+    const h_pad_px = h_px/30;
+    const x_range = this.props.x_range || [-10.0, 10.0];  
+    const y_range = this.props.y_range || [-10.0, 10.0]; 
+    const r = this.props.r || this.state.data_params.r || 1.0; 
+
+    const x_scale = d3.scaleLinear()
+      .domain([d3.min([x_range[0], -r]), d3.max([x_range[1], r])])
+      .range([w_pad_px, w_px - w_pad_px]);
+    const x_axis_component = d3.axisBottom()
+      .scale(x_scale) 
+    const y_scale = d3.scaleLinear()
+      .domain(y_range)
+      .range([h_px - h_pad_px, h_pad_px]);
+    const y_axis_component = d3.axisLeft()
+      .scale(y_scale); 
+
+    this.state.d3.svg.select(".x_axis")
+      .attr("transform", `translate(0,${h_px/2})`)
+      .call(x_axis_component)
+      .selectAll(".tick text")
+      .filter((elem) => elem=="0")
+      .attr("transform", `translate(-10,0)`);
+
+    this.state.d3.svg.select(".y_axis")
+      .attr("transform", `translate(${w_px/2},0)`)
+      .call(y_axis_component)
+      .selectAll(".tick text")
+      .filter((elem) => elem=="0")
+      .text("");
+  }
+
+  updateCircle(){
+    if(!this.state.d3.circle){
+      return; 
+    }
+    const r = this.props.r || this.state.data_params.r || 1.0; 
+    const w_px = this.props.w_px || this.state.data_params.w_px || 400; 
+    const h_px = this.props.h_px || this.state.data_params.h_px || 400; 
+    const w_pad_px = w_px/30; 
+    const x_range = this.state.data_params.x_range || [-10.0, 10.0]; 
+    const r_px = (r*(w_px-(2*w_pad_px)))/(x_range[1]-x_range[0]);
+    this.state.d3.circle
+      .attr("cx", w_px/2)
+      .attr("cy", h_px/2)
+      .attr("r", r_px)
+      .attr("fill-opacity", "0.0")
+      .attr("stroke", "red");
   }
 
   render() {
--- src/components/Posts/P1_11-jul-2021/P1_11-jul-2021.js
+++ src/components/Posts/P1_11-jul-2021/P1_11-jul-2021.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { MathComponent } from 'mathjax-react';
-import { Button, TextField } from '@material-ui/core'
+import { Button, TextField, Slider } from '@material-ui/core'
 import Post from '../Post/Post';
 import Circle from './Circle'
 
@@ -15,11 +15,27 @@ export default class P1_11Jul2021 extends Post{
                 r: 1,
                 x: [1],
                 y: [1]    
+            },
+            dimens:{
+                width: 0
             }
         }
     }
 
-    updateCircleR = (event) => {
+    componentDidMount(){
+        window.addEventListener('resize', this.windowResizeListener); 
+        this.setState({
+            dimens: {
+                width: this.container.offsetWidth
+            }
+        }); 
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener('resize', this.windowResizeListener); 
+    }
+
+    textFieldUpdateCircleR = (event) => {
         this.setState({
             circle: {
                 ...this.state.circle,
@@ -28,50 +44,68 @@ export default class P1_11Jul2021 extends Post{
         });
     }
 
-    calculateCirclePlot = (event) => {
-        var x = []; x[359] = 0; 
-        var y = []; y[359] = 0; 
-        for(var i = 0; i < 360; i++){
-            x[i] = this.state.circle.r*Math.cos(i*Math.PI / 180); 
-            y[i] = this.state.circle.r*Math.sin(i*Math.PI / 180); 
-        }
+    windowResizeListener = (event) => {
+        console.log(this.container.offsetWidth); 
+        this.setState({
+            dimens: {
+                width: this.container.offsetWidth
+            }
+        }); 
+    }
+
+    sliderUpdateCircleR = (event, newValue) => {
         this.setState({
             circle: {
                 ...this.state.circle,
-                x, 
-                y
+                r: newValue
             }
         });
     }
 
     renderContent(){
-        return <>
+        console.log(this.state.dimens.width)
+        return <div className="p1_11-jul-2021-container" ref={el => (this.container=el)}>
             <h1>Computing Pi</h1>
             <p>
             If you're like me, you learned about pi at some point in grade school as being a number
             that has to do with circles. Maybe you learned or memorized some common equations. 
             </p>
             <ul className="no-bullets">
-                <li>Diameter of a circle: <MathComponent tex={String.raw`D=2\pi r`} display={false}/></li>
+                <li>Diameter of a circle: <MathComponent tex={String.raw`C=2\pi r`} display={false}/></li>
                 <li>Area of circle: <MathComponent tex={String.raw`A=\pi r^2`} display={false}/></li>
                 <li>Surface area of a sphere: <MathComponent tex={String.raw`A=4 \pi r^2`} display={false}/></li>
                 <li>Volume of a sphere: <MathComponent tex={String.raw`V=\frac{4}{3}\pi r^3`} display={false}/></li>
             </ul>
             <p>
             Before getting into the actual computation of pi, let's explore those equations first. 
-            &lt;Post in progress&gt;
+            
             </p>
             <div className="graph-with-math">
                 <div className="math-input-fields">
-                    <TextField id="p1_11-jul-2021 r" label="r" onChange={this.updateCircleR}/>
-                    <Button id="p1_11-jul-2021 submit" onClick={this.calculateCirclePlot}>submit</Button>
+                    <TextField id="p1_11-jul-2021 r" label="r" onChange={this.textFieldUpdateCircleR}/>
+                    <Slider 
+                        defaultValue={1.0}
+                        min={0.0}
+                        step={0.1}
+                        max={10.0}
+                        valueLabelDisplay="auto" 
+                        onChange={this.sliderUpdateCircleR}
+                        />
                 </div>
-                <div className="math-input-fields">
+                <div className="graph-display">
                     <MathComponent tex={String.raw`x^2 + y^2 = r^2`} />
-                    <Circle r={this.state.circle.r}/>
+                    <Circle 
+                        r={this.state.circle.r} 
+                        w_px={Math.min(this.state.dimens.width, 400)} 
+                        h_px={Math.min(this.state.dimens.width, 400)}
+                        />
                 </div>
             </div>
-            <MathComponent tex={String.raw`\int_0^1 x^2\ dx`} />
-        </>    
+            <MathComponent tex={String.raw`C = 2\pi r`} />
+            <p>
+                This is pr
+                &lt;Post in progress&gt;
+            </p>
+        </div>    
     }
 } 
